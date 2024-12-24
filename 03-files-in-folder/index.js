59f1030ceb7a3435439b1cb706b06f74b0e7f4a7
Konstantin <woodprop@yandex.ru>
2024-12-24 23:10:28 +0300

    feat: add solution for task #03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,23 @@
 const { readdir } = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
 
 readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true })
   .then((files) => {
-    console.log(files);
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(__dirname, 'secret-folder', file.name);
+        const fileName = path.parse(filePath).name;
+        const fileExt = path.extname(filePath).slice(1);
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            throw new Error(err.message);
+          }
+          const fileSize = (stats.size / 1024).toFixed(3);
+          stdout.write(`${fileName} - ${fileExt} - ${fileSize}kb\n`);
+        });
+      }
+    });
   })
   .catch((error) => stdout.write(error.message));
