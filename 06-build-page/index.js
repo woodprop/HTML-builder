3745401a129157e3c9ec6138eccc1b61d2df7889
Konstantin <woodprop@yandex.ru>
2025-01-05 01:16:05 +0300

    feat: task #06 - add css bundle creation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,29 +1,48 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+const htmlTemplateFile = path.join(__dirname, 'template.html');
+const htmlComponentsFolder = path.join(__dirname, 'components');
+const distFolder = path.join(__dirname, 'project-dist');
+const htmlDistFile = path.join(distFolder, 'index.html');
+const cssDistFile = path.join(distFolder, 'style.css');
+
 async function buildPage() {
-  const distPath = path.join(__dirname, 'project-dist');
-  const templatePath = path.join(__dirname, 'template.html');
-  const resultPath = path.join(distPath, 'index.html');
-
-  await fs.mkdir(distPath, { recursive: true });
-
-  let pageTemplate = await fs.readFile(templatePath, 'utf-8');
+  await fs.mkdir(distFolder, { recursive: true }); //ToDO separate <----------------------------------------
+  let pageTemplate = await fs.readFile(htmlTemplateFile, 'utf-8');
 
   while (pageTemplate.indexOf('{{') !== -1) {
     const tagStartIdx = pageTemplate.indexOf('{{');
-    if (tagStartIdx === -1) return false;
     const tagEndIdx = pageTemplate.indexOf('}}', tagStartIdx);
-    if (tagEndIdx === -1) return false;
     const tag = pageTemplate.slice(tagStartIdx + 2, tagEndIdx);
-    const componentPath = path.join(__dirname, 'components', `${tag}.html`);
+    const componentPath = path.join(htmlComponentsFolder, `${tag}.html`);
     const componentContent = await fs.readFile(componentPath, 'utf-8');
     pageTemplate = pageTemplate.replace(`{{${tag}}}`, '\n' + componentContent);
   }
   // console.log(pageTemplate);
-  await fs.writeFile(resultPath, pageTemplate, 'utf-8');
+  await fs.writeFile(htmlDistFile, pageTemplate, 'utf-8');
 }
 
+async function createStyleBundle() {
+  const styles = [];
+  const stylesPath = path.join(__dirname, 'styles');
+  const files = (await fs.readdir(stylesPath, { withFileTypes: true })).filter(
+    (file) => {
+      return (
+        file.isFile() &&
+        path.extname(path.join(stylesPath, file.name)) === '.css'
+      );
+    },
+  );
+  // console.log(files);
 
+  for (const file of files) {
+    styles.push(await fs.readFile(path.join(stylesPath, file.name), 'utf-8'));
+  }
+
+  // console.log(styles);
+  await fs.writeFile(cssDistFile, styles, 'utf-8');
+}
 
 buildPage();
+createStyleBundle();
