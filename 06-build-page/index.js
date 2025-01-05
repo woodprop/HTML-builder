4451f07cb8c3a39c5b673173986a8f2e68d561f8
Konstantin <woodprop@yandex.ru>
2025-01-05 22:25:16 +0300

    feat: add solution for task #06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,12 +3,26 @@ const path = require('path');
 
 const htmlTemplateFile = path.join(__dirname, 'template.html');
 const htmlComponentsFolder = path.join(__dirname, 'components');
+const assetsFolder = path.join(__dirname, 'assets');
 const distFolder = path.join(__dirname, 'project-dist');
 const htmlDistFile = path.join(distFolder, 'index.html');
 const cssDistFile = path.join(distFolder, 'style.css');
 
-async function buildPage() {
-  await fs.mkdir(distFolder, { recursive: true }); //ToDO separate <----------------------------------------
+async function createDir() {
+  await fs
+    .access(distFolder)
+    .then(async () => {
+      const files = await fs.readdir(distFolder, { withFileTypes: true });
+      for (const f of files) {
+        await fs.rm(path.join(distFolder, f.name), { recursive: true });
+      }
+    })
+    .catch(async () => {
+      await fs.mkdir(distFolder, { recursive: true });
+    });
+}
+
+async function createHtml() {
   let pageTemplate = await fs.readFile(htmlTemplateFile, 'utf-8');
 
   while (pageTemplate.indexOf('{{') !== -1) {
@@ -19,7 +33,7 @@ async function buildPage() {
     const componentContent = await fs.readFile(componentPath, 'utf-8');
     pageTemplate = pageTemplate.replace(`{{${tag}}}`, '\n' + componentContent);
   }
-  // console.log(pageTemplate);
+
   await fs.writeFile(htmlDistFile, pageTemplate, 'utf-8');
 }
 
@@ -34,15 +48,37 @@ async function createStyleBundle() {
       );
     },
   );
-  // console.log(files);
 
   for (const file of files) {
     styles.push(await fs.readFile(path.join(stylesPath, file.name), 'utf-8'));
   }
 
-  // console.log(styles);
   await fs.writeFile(cssDistFile, styles, 'utf-8');
 }
 
-buildPage();
-createStyleBundle();
+async function copyDir(src, dest = distFolder) {
+  const newDirName = path.parse(src).name;
+  const newDirPath = path.join(dest, newDirName);
+  await fs.mkdir(newDirPath, { recursive: true });
+  const files = await fs.readdir(src, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      await copyDir(path.join(src, file.name), newDirPath);
+    } else {
+      await fs.copyFile(
+        path.join(src, file.name),
+        path.join(newDirPath, file.name),
+      );
+    }
+  }
+}
+
+async function build() {
+  await createDir();
+  await createHtml();
+  await createStyleBundle();
+  await copyDir(assetsFolder);
+}
+
+build().catch((error) => console.log(error.message));
